235ca13f339712ad4c7c4e645c72f87265773b6f
mantasdev <32267238+mantasdev1@users.noreply.github.com>
2018-05-10 12:27:03 +0100

    awd

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,3 @@
-var map;
-function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
-});
-}
-	  
 $(document).on("pageshow","#toDoPage", onPageShow);
 
 function onPageShow() {
