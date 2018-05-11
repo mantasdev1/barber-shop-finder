61c7c48b0c7bac9582cb558c354c8b52a2db829c
mantasdev <32267238+mantasdev1@users.noreply.github.com>
2018-05-11 18:25:26 +0100

    awd

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,43 +1,46 @@
+var map;
+var places;
+var start = {lat: 52.1936, lng: -2.223981};
+
 var options = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0
 };
 
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
 
 function onSuccess(position) {
-	var map;
-	var start = {lat: position.coords.latitude, lng: position.coords.longitude};
-
-	function loadMap() {
-		var mapOptions = {
-			zoom: 16,
-			center: start,
-			mapTypeControl: false,
-			navigationalControl: false,
-			disableDefaultUI: true
-		}
-	
-		map = new google.maps.Map(document.getElementById("map"), mapOptions);
-		places = new google.maps.places.PlacesService(map);
-		
-		var marker = new google.maps.Marker({
-			position: start,
-			animation: google.maps.Animation.BOUNCE
-		});
-		
-		marker.setMap(map);
-
-		$("#map").css("height", $(window).innerHeight());
-	}
-	
+  start = {lat: position.coords.latitude, lng: position.coords.longitude};
 	loadMap();
-	
 	console.log("Your GEO location is: " + position.coords.latitude + "," + position.coords.longitude);
 }
 
 function onError(error) {
-console.log('code: ' + error.code + 'n' +
-'message: ' + error.message + 'n');
+  loadMap();
+  console.log('code: ' + error.code + 'n' + 'message: ' + error.message + 'n');
+}
+
+function loadMap() {
+  var mapOptions = {
+    zoom: 16,
+    center: start,
+    mapTypeControl: false,
+    navigationalControl: false,
+    disableDefaultUI: true
+  }
+
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  places = new google.maps.places.PlacesService(map);
+
+  var marker = new google.maps.Marker({
+    position: start,
+    animation: google.maps.Animation.BOUNCE
+  });
+
+  marker.setMap(map);
+
+  $("#map").css("height", $(window).innerHeight());
 }
