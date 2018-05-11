ebddf5e37052d0448ef171c6f4efb54d1f185480
mantasdev <32267238+mantasdev1@users.noreply.github.com>
2018-05-11 18:03:32 +0100

    awdawd

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,35 +1,43 @@
-var map;
-var start = {lat: 0, lng: 0};
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
 navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
 function onSuccess(position) {
-	start = {lat: position.coords.latitude, lng: position.coords.longitude};
-	alert(start);
+	var map;
+	var start = {lat: position.coords.latitude, lng: position.coords.longitude};
+
+	function loadMap() {
+		var mapOptions = {
+			zoom: 16,
+			center: start,
+			mapTypeControl: false,
+			navigationalControl: false,
+			disableDefaultUI: true
+		}
+	
+		map = new google.maps.Map(document.getElementById("map"), mapOptions);
+		places = new google.maps.places.PlacesService(map);
+		
+		var marker = new google.maps.Marker({
+			position: start,
+			animation: google.maps.Animation.BOUNCE
+		});
+		
+		marker.setMap(map);
+
+		$("#map").css("height", $(window).innerHeight());
+	}
+	
+	loadMap();
+	
+	console.log("Your GEO location is: " + position.coords.latitude + "," + position.coords.longitude);
 }
 
 function onError(error) {
-alert('code: ' + error.code + 'n' +
+console.log('code: ' + error.code + 'n' +
 'message: ' + error.message + 'n');
-}
-
-function loadMap() {
-	
-	var mapOptions = {
-		zoom: 16,
-		center: start,
-		mapTypeControl: false,
-		navigationalControl: false,
-		disableDefaultUI: true
-	}
-
-	map = new google.maps.Map(document.getElementById("map"), mapOptions);
-	places = new google.maps.places.PlacesService(map);
-
-	$("#map").css("height", $(window).innerHeight());
-	
-	var marker = new google.maps.Marker({
-          center: start,
-          map: map,
-          title: 'Hello World!'
-     });
 }
